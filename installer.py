eabd1886b4e474609d47a92dfca0d3dbd3a04085
Appitome <mcrobby@me.com>
2023-02-13 01:50:16 -0500

    streamlined rgbmatrix

--- installer.py
+++ installer.py
@@ -1,6 +1,8 @@
-import sys
-import subprocess
 import os
+import sys
+import shutil
+import requests
+import subprocess
 
 
 def suto(package):
@@ -31,8 +33,32 @@ def install(package):
  
 install("Pillow")
 #install("rpi_ws281x")
-#install("neopixel")
+#install("neopixel") 
+
+#Install matrix files
+response = requests.get("https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/main/rgb-matrix.sh")
+with open("rgb-matrix.sh", "w") as file:
+    file.write(response.text)
+subprocess.run(["sudo", "bash", "rgb-matrix.sh"])
+
+#Extract rgbmatrix file
+if os.path.exists('rgbmatrix') == False:
+
+    src_path = 'rpi-rgb-led-matrix/bindings/python/rgbmatrix'
+    dst_path = os.path.join(os.getcwd(), os.path.basename(src_path))
+
+    shutil.copytree(src_path, dst_path)
+else: 
+    print('rgbmatrix found')
+
+#Remove extra matrix files
+if os.path.exists('rgb-matrix.sh'):
+    os.remove('rgb-matrix.sh')
+if os.path.exists('rpi-rgb-led-matrix'):
+    shutil.rmtree('rpi-rgb-led-matrix')
+
 
 #Reboot once all modules are installed
 print('Rebooting System')
-os.system("sudo reboot")
+print('Please wait a few minutes before trying to connect to device')
+os.system("sudo reboot") 
